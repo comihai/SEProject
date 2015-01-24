e0e631958c2bb806d8f54aeb4b1f60b9a3ca262b
comihai <coca.mihai5@gmail.com>
2015-01-24 18:11:51 +0200

    added DataBase abstract class and DatabaseResult interface for saving the score

---
+++ core/src/com/hardestfield/game/interfaces/DatabaseResult.java
@@ -0,0 +1,12 @@
+package com.hardestfield.game.interfaces;
+
+/**
+ * Created by mihai on 1/24/2015.
+ * THis interface will be implemented on both Android and Desktop applications
+ */
+public interface DatabaseResult {
+    public boolean isEmpty();
+    public boolean moveToNext();
+    public int getColumnIndex(String name);
+    public float getFloat(int columnIndex);
+}
---
+++ core/src/com/hardestfield/game/utils/DataBase.java
@@ -0,0 +1,34 @@
+package com.hardestfield.game.utils;
+
+import com.hardestfield.game.interfaces.DatabaseResult;
+
+/**
+ * Created by mihai on 1/24/2015.
+ * THis class that needs to be implemented on Android and Desktop applications
+ */
+public abstract class DataBase implements DatabaseResult{
+
+    protected static String databaseName="HardestField";
+    protected static DataBase instance = null;
+    protected static int version=1;
+
+    //Runs a sql query like "create".
+    public abstract void execute(String sql);
+
+    //Identical to execute but returns the number of rows affected (useful for updates)
+    public abstract int executeUpdate(String sql);
+
+    //Runs a query and returns an Object with all the results of the query. [Result Interface is defined below]
+    public abstract DatabaseResult query(String sql);
+
+    public void onCreate(){
+        execute("CREATE TABLE 'highscores' ('_id' INTEGER PRIMARY KEY  NOT NULL , 'name' VARCHAR NOT NULL , 'score' INTEGER NOT NULL );");
+        execute("INSERT INTO 'highscores'(name,score) values ('Mihai',2000)");
+    }
+
+    public void onUpgrade(){
+        execute("DROP TABLE IF EXISTS 'highscores';");
+        onCreate();
+    }
+
+}
